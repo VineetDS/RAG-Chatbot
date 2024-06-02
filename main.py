6252cee1fed34e4c4ff19ba45fbab5364f489380
VineetDS <98698202+VineetDS@users.noreply.github.com>
2024-06-02 22:09:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 import os
 
+#Google API_Key
 os.environ['GOOGLE_API_KEY'] =  'API_KEY'
 
+# Loading Pdf and extracting text from it
 def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
@@ -19,44 +21,55 @@ def get_pdf_text(pdf_docs):
             text+= page.extract_text()
     return  text
 
+#splitting texts into smaller chunks
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
     chunks = text_splitter.split_text(text)
     return chunks
 
+# Creating vector store using GoogkePalm embeddings
 def get_vector_store(text_chunks):
     embeddings = GooglePalmEmbeddings()
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     return vector_store
 
+#creating conversational chain for user
 def get_conversational_chain(vector_store):
     llm=GooglePalm()
     memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
     return conversation_chain
 
+# user input and display conversation
 def user_input(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chatHistory = response['chat_history']
     for i, message in enumerate(st.session_state.chatHistory):
         if i%2 == 0:
-            st.write("Human: ", message.content)
+            st.write("Message: ", message.content)
         else:
-            st.write("Bot: ", message.content)
+            st.write("LLM: ", message.content)
+
+# Main function for streamlit application
 def main():
     st.set_page_config("Chat with Multiple PDFs")
     st.header("Chat with Multiple PDF 💬")
     user_question = st.text_input("Ask a Question from the PDF Files")
+    # intitializing session
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chatHistory" not in st.session_state:
         st.session_state.chatHistory = None
     if user_question:
         user_input(user_question)
+
+    # sidebar navigation
     with st.sidebar:
         st.title("Settings")
         st.subheader("Upload your Documents")
         pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
+
+        # Uploading Pdfs file
         if st.button("Process"):
             with st.spinner("Processing"):
                 raw_text = get_pdf_text(pdf_docs)
